1917d4e6b8ed4c93a80ebc37b678a15b19ab93da
Ruben Verborgh <ruben@verborgh.org>
2014-09-11 20:35:57 +0200

    Add support for static assets.

--- test/server/LinkedDataFragmentsServer-test.js
+++ test/server/LinkedDataFragmentsServer-test.js
@@ -1,6 +1,7 @@
 var LinkedDataFragmentsServer = require('../../lib/server/LinkedDataFragmentsServer');
 
-var request = require('supertest');
+var request = require('supertest'),
+    fs = require('fs');
 
 describe('LinkedDataFragmentsServer', function () {
   describe('A LinkedDataFragmentsServer instance', function () {
@@ -26,6 +27,33 @@ describe('LinkedDataFragmentsServer', function () {
       client.options('/').expect(function (response) {
         response.should.have.property('statusCode', 200);
         response.should.have.property('text', '');
+      }).end(done);
+    });
+
+    it('should correctly serve SVG assets', function (done) {
+      client.get('/assets/logo').expect(function (response) {
+        var asset = fs.readFileSync(__dirname + '/../../assets/logo.svg', 'utf8');
+        response.should.have.property('statusCode', 200);
+        response.headers.should.have.property('content-type', 'image/svg+xml');
+        response.should.have.property('text', asset);
+      }).end(done);
+    });
+
+    it('should correctly serve CSS assets', function (done) {
+      client.get('/assets/style').expect(function (response) {
+        var asset = fs.readFileSync(__dirname + '/../../assets/style.css', 'utf8');
+        response.should.have.property('statusCode', 200);
+        response.headers.should.have.property('content-type', 'text/css');
+        response.should.have.property('text', asset);
+      }).end(done);
+    });
+
+    it('should correctly serve ICO assets', function (done) {
+      client.get('/favicon.ico').expect(function (response) {
+        var asset = fs.readFileSync(__dirname + '/../../assets/favicon.ico', 'utf8');
+        response.should.have.property('statusCode', 200);
+        response.headers.should.have.property('content-type', 'image/x-icon');
+        response.should.have.property('text', asset);
       }).end(done);
     });
   });
