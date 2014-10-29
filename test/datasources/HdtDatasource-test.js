7124e51b50755c11af65b5ef6f92b17afe5d3236
Ruben Verborgh <ruben@verborgh.org>
2014-10-29 11:37:44 +0100

    Translate blank nodes in triples to IRIs.

--- test/datasources/HdtDatasource-test.js
+++ test/datasources/HdtDatasource-test.js
@@ -2,7 +2,8 @@ var Datasource = require('../../lib/datasources/Datasource'),
     HdtDatasource = require('../../lib/datasources/HdtDatasource'),
     path = require('path');
 
-var exampleHdtFile = path.join(__dirname, '../../node_modules/hdt/test/test.hdt');
+var exampleHdtFile = path.join(__dirname, '../assets/test.hdt');
+var exampleHdtFileWithBlanks = path.join(__dirname, '../assets/test-blank.hdt');
 
 describe('HdtDatasource', function () {
   describe('The HdtDatasource module', function () {
@@ -78,15 +79,52 @@ describe('HdtDatasource', function () {
       { object: 'http://example.org/s1',    limit: 10, features: { triplePattern: true, limit: true } },
       0, 0);
   });
+
+  describe('A HdtDatasource instance with blank nodes', function () {
+    var datasource = new HdtDatasource({ file: exampleHdtFileWithBlanks });
+    after(function (done) { datasource.close(done); });
+
+    itShouldExecute(datasource,
+      'the empty query',
+      { features: { triplePattern: true } },
+      6, 6,
+      [
+        { subject: 'genid:a', predicate: 'b', object: 'c1' },
+        { subject: 'genid:a', predicate: 'b', object: 'c2' },
+        { subject: 'genid:a', predicate: 'b', object: 'c3' },
+        { subject: 'a',       predicate: 'b', object: 'genid:c1' },
+        { subject: 'a',       predicate: 'b', object: 'genid:c2' },
+        { subject: 'a',       predicate: 'b', object: 'genid:c3' },
+      ]);
+  });
+
+  describe('A HdtDatasource instance with blank nodes and a blank node prefix', function () {
+    var datasource = new HdtDatasource({ file: exampleHdtFileWithBlanks,
+                                         blankNodePrefix: 'http://example.org/.well-known/genid/' });
+    after(function (done) { datasource.close(done); });
+
+    itShouldExecute(datasource,
+      'the empty query',
+      { features: { triplePattern: true } },
+      6, 6,
+      [
+        { subject: 'http://example.org/.well-known/genid/a', predicate: 'b', object: 'c1' },
+        { subject: 'http://example.org/.well-known/genid/a', predicate: 'b', object: 'c2' },
+        { subject: 'http://example.org/.well-known/genid/a', predicate: 'b', object: 'c3' },
+        { subject: 'a', predicate: 'b', object: 'http://example.org/.well-known/genid/c1' },
+        { subject: 'a', predicate: 'b', object: 'http://example.org/.well-known/genid/c2' },
+        { subject: 'a', predicate: 'b', object: 'http://example.org/.well-known/genid/c3' },
+      ]);
+  });
 });
 
-function itShouldExecute(datasource, name, query, expectedResultsCount, totalCount) {
+function itShouldExecute(datasource, name, query, expectedResultsCount, totalCount, expectedTriples) {
   describe('executing ' + name, function () {
-    var resultsCount = 0, totalCount;
+    var resultsCount = 0, totalCount, triples = [];
     before(function (done) {
       var result = datasource.select(query);
       result.on('metadata', function (metadata) { totalCount = metadata.totalCount; });
-      result.on('data', function () { resultsCount++; });
+      result.on('data', function (triple) { resultsCount++; expectedTriples && triples.push(triple); });
       result.on('end', done);
     });
 
@@ -97,5 +135,13 @@ function itShouldExecute(datasource, name, query, expectedResultsCount, totalCou
     it('should emit the expected total number of triples', function () {
       expect(totalCount).to.equal(totalCount);
     });
+
+    if (expectedTriples) {
+      it('should emit the expected triples', function () {
+        expect(triples.length).to.equal(expectedTriples.length);
+        for (var i = 0; i < expectedTriples.length; i++)
+          triples[i].should.deep.equal(expectedTriples[i]);
+      });
+    }
   });
 }
