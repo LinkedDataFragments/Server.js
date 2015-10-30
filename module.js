063fb55b0f2abe375f3fb4d37620b9aeeb8bd104
Ruben Verborgh <ruben@verborgh.org>
2015-10-30 13:50:18 +0100

    Export all components.
    This makes the ldf-server package usable as a submodule.
    Developers can now do `require('ldf-server').datasources.Datasource`.

---
+++ module.js
@@ -0,0 +1,28 @@
+/*! @license ©2015 Ruben Verborgh - Multimedia Lab / iMinds / Ghent University */
+
+/** Exports of the ldf-server package for use as a submodule (as opposed to standalone) */
+
+var fs = require('fs'),
+    path = require('path');
+
+// Export all modules in the `lib` folder
+module.exports = readModules(path.join(__dirname, 'lib'));
+
+// Reads modules in the given folder and subfolders
+function readModules(folder) {
+  var modules = {};
+  fs.readdirSync(folder).forEach(function (name) {
+    var location = path.join(folder, name), item = fs.statSync(location);
+    // Add script files by including them
+    if (item.isFile()) {
+      var scriptMatch = name.match(/(\w+)\.js$/);
+      if (scriptMatch)
+        modules[scriptMatch[1]] = require(location);
+    }
+    // Add folders by recursing over them
+    else if (item.isDirectory()) {
+      modules[name] = readModules(location);
+    }
+  });
+  return modules;
+}
