LinkedDataFragments/Server.js

commit 20b46c9557134ba9cd7b392726ad0306e30c4812

Fix time gate paths with trailing slash.

lib/controllers/TimegateController.js
@@ -36,15 +36,15 @@ function TimegateController(options) {
   this._routers = options.routers || [];
 
   // Set up path matching
-  this._timegatePath = timegates.baseUrl  || '/timegate/',
-  this._matcher = new RegExp('^' + Util.toRegExp(this._timegatePath) + '((.+?)\\?|(.+))');
+  this._timegatePath = timegates.baseUrl || '/timegate/',
+  this._matcher = new RegExp('^' + Util.toRegExp(this._timegatePath) + '(.+?)\/?$');
 }
 Controller.extend(TimegateController);
 
 // Tries to serve the requested Timegate
 TimegateController.prototype._handleRequest = function (request, response, next) {
   var self = this, timegateMatch = this._matcher && this._matcher.exec(request.url),
-  datasource = timegateMatch && (timegateMatch[2] || timegateMatch[1]);
+      datasource = timegateMatch && timegateMatch[1];
   if (datasource && this._timemaps[datasource]) {
     // retrieve Accept-Datetime & construct memento link
     var acceptDatetime = toDate(request.headers['accept-datetime']), // If no datetime is present, return most recent one

commit 1dd81460ce7d89d5169bc856500cd86103ca2c71

Re-enable query string on timegate.
This ability got broken by 20b46c9557134ba9cd7b392726ad0306e30c4812.

lib/controllers/TimegateController.js
@@ -37,7 +37,7 @@ function TimegateController(options) {
 
   // Set up path matching
   this._timegatePath = timegates.baseUrl || '/timegate/',
-  this._matcher = new RegExp('^' + Util.toRegExp(this._timegatePath) + '(.+?)\/?$');
+  this._matcher = new RegExp('^' + Util.toRegExp(this._timegatePath) + '(.+?)\/?(?:\\?.*)?$');
 }
 Controller.extend(TimegateController);
 