5fd4e57a591dcf5e06e2757accaab47f401716ee
Ruben Verborgh <ruben@verborgh.org>
2016-10-11 16:06:35 +0200

    Generate test certificates for localhost (no port).

--- config/certs/make-server-certificates.sh
+++ config/certs/make-server-certificates.sh
@@ -8,14 +8,14 @@
 openssl genrsa -out localhost-ca.key 2048
 
 # Self-sign your Certificate Authority
-openssl req -x509 -new -nodes -key localhost-ca.key -days 365 -out localhost-ca.crt -subj "/C=BE/ST=OVL/L=Ghent/O=MyOrganization/CN=localhost:3000" -config ./webid.cnf
+openssl req -x509 -new -nodes -key localhost-ca.key -days 365 -out localhost-ca.crt -subj "/C=BE/ST=OVL/L=Ghent/O=MyOrganization/CN=localhost" -config ./webid.cnf
 
 # Create a server certificate 
 # NOTE: You MUST match CN to the domain name or ip address you want to use
 openssl genrsa -out localhost-server.key 2048
 
 # Create a certificate request for the server, which your CA will sign
-openssl req -new -key localhost-server.key -out localhost-server.csr -subj "/C=BE/ST=OVL/L=Ghent/O=MyOrganization/CN=localhost:3000" -config ./webid.cnf
+openssl req -new -key localhost-server.key -out localhost-server.csr -subj "/C=BE/ST=OVL/L=Ghent/O=MyOrganization/CN=localhost" -config ./webid.cnf
 
 # Sign the request with your CA
 openssl x509  -req -in localhost-server.csr -CA localhost-ca.crt -CAkey localhost-ca.key -CAcreateserial -out localhost-server.crt -days 365
