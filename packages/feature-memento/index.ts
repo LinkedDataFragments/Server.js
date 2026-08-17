a759d197f099e3c257e8b7e5b9721129549ea68d
Michiel <michiel.vdvelde@outlook.com>
2026-08-17 22:11:09 +0000

    Add per-subdirectory index.ts files to feature-memento, matching other packages

--- packages/feature-memento/index.ts
+++ packages/feature-memento/index.ts
@@ -1,18 +1,5 @@
 /*! @license MIT ©2015-2016 Ruben Verborgh, Ghent University - imec */
 /* Exports of the components of this package */
 
-import { TimegateController } from './lib/controllers/TimegateController';
-import { MementoControllerExtension } from './lib/controllers/MementoControllerExtension';
-import { MementoHtmlViewExtension } from './lib/views/memento/QuadPatternFragmentsHtmlView-Memento';
-
-module.exports = {
-  controllers: {
-    TimegateController,
-    MementoControllerExtension,
-  },
-  views: {
-    memento: {
-      'QuadPatternFragmentsHtmlView-Memento': MementoHtmlViewExtension,
-    },
-  },
-};
+export * as controllers from './lib/controllers';
+export * as views from './lib/views';
---
+++ packages/feature-memento/lib/views/memento/index.ts
@@ -0,0 +1,8 @@
+/*! @license MIT ©2015-2016 Ruben Verborgh, Ghent University - imec */
+/* Exports of the memento views of this package */
+
+import { MementoHtmlViewExtension } from './QuadPatternFragmentsHtmlView-Memento';
+
+module.exports = {
+  'QuadPatternFragmentsHtmlView-Memento': MementoHtmlViewExtension,
+};
