d571680b67a8c8c6c5962b17018448a810f137ab
Michiel Van de Velde <michiel.vdvelde@outlook.com>
2026-08-18 16:06:34 +0200

    Apply suggestion from @jitsedesmet
    Co-authored-by: Jitse De Smet <35114273+jitsedesmet@users.noreply.github.com>

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,28 +1,13 @@
 /*! @license MIT ©2015-2016 Ruben Verborgh, Ghent University - imec */
 /* Exports of the components of this package */
 
-import * as controllers from './lib/controllers';
-import * as datasources from './lib/datasources';
-import * as routers from './lib/routers';
-import * as views from './lib/views';
-
-import { runCli, runCustom } from './lib/CliRunner';
-import { LinkedDataFragmentsServer } from './lib/LinkedDataFragmentsServer';
-import { LinkedDataFragmentsServerWorker } from './lib/LinkedDataFragmentsServerWorker';
-import { UrlData } from './lib/UrlData';
-import * as Util from './lib/Util';
-
-const Core = {
-  controllers,
-  datasources,
-  routers,
-  views,
-  runCli,
-  runCustom,
-  LinkedDataFragmentsServer,
-  LinkedDataFragmentsServerWorker,
-  UrlData,
-  Util,
-};
-
-export = Core;
+export * as controllers from './lib/controllers';
+export * as datasources from './lib/datasources';
+export * as routers from './lib/routers';
+export * as views from './lib/views';
+export { runCli, runCustom } from './lib/CliRunner';
+export { LinkedDataFragmentsServer } from './lib/LinkedDataFragmentsServer';
+export { LinkedDataFragmentsServerWorker } from './lib/LinkedDataFragmentsServerWorker';
+export { UrlData } from './lib/UrlData';
+export * as Util from './lib/Util';
+export * from './lib/types';
